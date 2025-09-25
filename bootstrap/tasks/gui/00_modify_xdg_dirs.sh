ce9c46b28df6528bc9d70be7a32faabd0353dbbc
at337am <lueqit@gmail.com>
2025-09-26 05:42:16 +0800

    WIP

---
+++ bootstrap/tasks/gui/00_modify_xdg_dirs.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+LC_ALL=C xdg-user-dirs-update --force
+
+CONFIG_FILE="$HOME/.config/user-dirs.dirs"
+
+if [[ ! -f "$CONFIG_FILE" ]]; then
+    echo "Error: 配置文件 '$CONFIG_FILE' 不存在"
+    exit 1
+fi
+
+sed -i \
+    -e 's#XDG_DESKTOP_DIR="$HOME/Desktop"#XDG_DESKTOP_DIR="$HOME"#' \
+    -e 's#XDG_TEMPLATES_DIR="$HOME/Templates"#XDG_TEMPLATES_DIR="$HOME"#' \
+    -e 's#XDG_PUBLICSHARE_DIR="$HOME/Public"#XDG_PUBLICSHARE_DIR="$HOME"#' \
+    "$CONFIG_FILE"
+
+if [[ $? -eq 0 ]]; then
+    echo "配置文件 '$CONFIG_FILE' 已成功更新"
+    command rm -rfv ~/Templates ~/Public ~/Desktop
+    exit 0
+else
+    echo "Error: 文件更新失败"
+    exit 1
+fi
