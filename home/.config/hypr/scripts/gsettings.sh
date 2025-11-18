f54a7dd8a9f7dfc229e3b692db33d31098a5d90a
at337am <lueqit@gmail.com>
2025-11-18 15:22:17 +0800

    WIP

---
+++ home/.config/hypr/scripts/gsettings.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# 主题名称 = 目录名称
+gsettings set org.gnome.desktop.interface cursor-theme 'Breeze_Light'
+gsettings set org.gnome.desktop.interface cursor-size 24
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.0
+gsettings set org.gnome.desktop.interface font-name 'Inter 12'
+gsettings set org.gnome.desktop.interface gtk-theme 'Orchis-Pink-Light'
+gsettings set org.gnome.desktop.interface icon-theme 'Tela-grey'
