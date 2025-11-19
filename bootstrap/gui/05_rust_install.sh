2540d8b36c2633c984a1d4de210f5f9f3a68b21c
at337am <lueqit@gmail.com>
2025-11-19 12:13:45 +0800

    WIP

--- bootstrap/gui/05_rust_install.sh
+++ bootstrap/gui/05_rust_install.sh
@@ -6,6 +6,8 @@ cargo install cargo-cache
 
 cargo install xh
 
+# wl-clip-persist: https://github.com/Linus789/wl-clip-persist
+
 tmp_dir=$(mktemp -d)
 trap 'command rm -rf "$tmp_dir"' EXIT
 
@@ -13,7 +15,9 @@ git clone --depth 1 https://github.com/Linus789/wl-clip-persist "$tmp_dir/wl-cli
 
 (
     cd "$tmp_dir/wl-clip-persist"
-    cargo install --path . --force
+    cargo build --release
 )
+
+sudo install -m 755 "$tmp_dir/wl-clip-persist/target/release/wl-clip-persist" /usr/local/bin/
 
 printf "Done.\n"
