90d725f67fca86e9a2698f5b538c573bbe67ff53
at337am <lueqit@gmail.com>
2025-11-21 16:27:25 +0800

    WIP

--- tidy/syncs.sh
+++ tidy/syncs.sh
@@ -33,7 +33,7 @@ mkdir -p "$TARGET_DIR"
 # -------------- 主要逻辑 --------------
 
 # 只做同步
-data_mirroring() {
+mirroring() {
     for path in "${source_dirs[@]}"; do
         rsync -a --delete \
             "$path/" \
@@ -41,35 +41,64 @@ data_mirroring() {
     done
 }
 
-# 日常备份, 打包 proj 项目
-pack_proj(){
-    cd "$TARGET_DIR"
-    mkdir -p ~/Downloads/syncs_proj
-    cp -a ./dev ./Documents ~/Downloads/syncs_proj
-    cd ~/Downloads
-    tar -cf "syncs_proj_$(date +"%y%m%d_%H%M%S").tar" syncs_proj
-    command rm -rf syncs_proj
-}
-
 # 迁移, 打包所有内容
-pack_migration() {
+pack_all() {
     tar -cf "$HOME/Downloads/syncs_migration_$(date +"%y%m%d_%H%M%S").tar" -C /data/bak/ syncs
 }
 
+# 日常备份, 打包 proj 项目
+pack_proj(){
+    cd "$TARGET_DIR"
+    mkdir -p ~/Downloads/proj_bak
+    cp -a dev Documents ~/Downloads/proj_bak
+    cd ~/Downloads
+    tar -cf "proj_bak_$(date +"%y%m%d_%H%M%S").tar" proj_bak
+    command rm -rf proj_bak
+}
+
+# 日常备份, 打包字体
+pack_fonts() {
+    cd "$TARGET_DIR"
+    cp -a fonts ~/Downloads
+    cd ~/Downloads
+    tar -cf "fonts_bak_$(date +"%y%m%d_%H%M%S").tar" fonts
+    command rm -rf fonts
+}
+
+# 日常备份, 打包照片
+pack_pfp() {
+    cd "$TARGET_DIR"
+    cp -a PFP ~/Downloads
+    cd ~/Downloads
+    tar -cf "PFP_bak_$(date +"%y%m%d_%H%M%S").tar" PFP
+    command rm -rf PFP
+}
+
+pack_restore() {
+    cd "$TARGET_DIR"
+    cp -a restore ~/Downloads
+    cd ~/Downloads
+    tar -cf "restore_bak_$(date +"%y%m%d_%H%M%S").tar" restore
+    command rm -rf restore
+}
+
 if [[ "$#" -eq 0 ]]; then
-    data_mirroring
-elif [[ "$#" -eq 1 && "$1" == "-p" ]]; then
-    data_mirroring
+    mirroring
+elif [[ "$#" -eq 1 && "$1" == "--proj" ]]; then
+    mirroring
     pack_proj
-# elif [[ "$#" -eq 1 && "$1" == "--mig" ]]; then
-#     data_mirroring
-# elif [[ "$#" -eq 1 && "$1" == "--mig" ]]; then
-#     data_mirroring
-# elif [[ "$#" -eq 1 && "$1" == "--mig" ]]; then
-#     data_mirroring
-elif [[ "$#" -eq 1 && "$1" == "--mig" ]]; then
-    data_mirroring
-    pack_migration
+elif [[ "$#" -eq 1 && "$1" == "--fonts" ]]; then
+    mirroring
+    pack_fonts
+elif [[ "$#" -eq 1 && "$1" == "--pfp" ]]; then
+    mirroring
+    pack_pfp
+elif [[ "$#" -eq 1 && "$1" == "--restore" ]]; then
+    mirroring
+    pack_restore
+elif [[ "$#" -eq 1 && "$1" == "--all" ]]; then
+    mirroring
+    pack_all
 else
 	printf "Error: Invalid arguments.\n" >&2
     printf "Usage: %s [-p | --mig]\n" "$(basename "$0")" >&2
