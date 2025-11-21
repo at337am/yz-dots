0539066a97be11ffb530b3f1d57141867644507b
at337am <lueqit@gmail.com>
2025-11-21 12:57:29 +0800

    WIP

--- tidy/syncs.sh
+++ tidy/syncs.sh
@@ -1,12 +1,34 @@
 #!/usr/bin/env bash
 
 
+# 之后在重新写 bootstrap 脚本时，全部用统一用 ./ 执行不要用 source
+
 # todo:
+
+# ~/.ssh
+# ~/.local/share/fonts
+# ~/Pictures/PFP
+# ~/Documents
+# /data/restore
+
+
+
+
+# ~/workspace/dev/yz-dots
+
+
 
 # 执行 Migration 表示所有数据进行迁移
 # 执行 bak 表示备份日常数据
 
+PFP_PATH="$HOME/Pictures"
 
+mkdir 
+
+migration() {
+
+    
+}
 
 target_dir="/data/bak/syncs"
 
@@ -19,17 +41,19 @@ sync_projects() {
 
     rsync -a --delete \
         "$HOME/workspace/dev/" \
-        "$target_dir/projects/"
+        "$target_dir/dev/"
 }
 
-bak_syncs() {
+pack_projects() {
     tar -cf "$HOME/Downloads/syncs_bak_$(date +"%y%m%d_%H%M%S").tar" -C /data/bak/ syncs
 }
+
+
 
 if [[ "$#" -eq 0 ]]; then
     sync_projects
 elif [[ "$#" -eq 1 && "$1" == "bak" ]]; then
-    bak_syncs
+    pack_projects
 else
     printf "参数错误\n" >&2
     printf "用法: syncs.sh [bak]\n" >&2
