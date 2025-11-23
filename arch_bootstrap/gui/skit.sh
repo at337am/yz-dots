53bf943680c59d797716f6a970e81f3456b55628
at337am <lueqit@gmail.com>
2025-11-23 19:42:45 +0800

    WIP

--- arch_bootstrap/gui/skit.sh
+++ arch_bootstrap/gui/skit.sh
@@ -2,11 +2,14 @@
 
 set -euo pipefail
 
-# cd ~/Downloads
-# git clone git@github.com:at337am/notes.git
-# git clone git@github.com:at337am/yz-dots.git
-# git clone git@github.com:at337am/skit.git
-# git clone git@github.com:at337am/raindrop.git
+# git_clone() {
+#     mkdir -p ~/Downloads/git_clone_tmp
+#     cd ~/Downloads/git_clone_tmp
+#     git clone git@github.com:at337am/notes.git
+#     git clone git@github.com:at337am/yz-dots.git
+#     git clone git@github.com:at337am/skit.git
+#     git clone git@github.com:at337am/raindrop.git
+# }
 
 if [[ ! -d "$HOME/workspace/dev/skit" ]]; then
     printf "Error: skit directory does not exist!" >&2
