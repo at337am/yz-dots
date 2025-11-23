8a02fb53a15b84917da4db9f19aa2f43d1882e27
at337am <lueqit@gmail.com>
2025-11-23 18:54:47 +0800

    WIP

--- arch_bootstrap/setup/install_themes.sh
+++ arch_bootstrap/setup/install_themes.sh
@@ -19,9 +19,7 @@ mkdir -p ~/.local/share/icons
 gtk_themes(){
     # 卸载: ./install.sh --uninstall
     git clone --depth 1 https://github.com/vinceliuice/Orchis-theme.git "$tmp_dir/Orchis-theme"
-
     cd "$tmp_dir/Orchis-theme"
-
     ./install.sh \
         --theme pink \
         --color light \
@@ -32,11 +30,8 @@ gtk_themes(){
 icon_themes() {
     # 卸载: ./install.sh -u
     git clone --depth 1 https://github.com/vinceliuice/Tela-icon-theme.git "$tmp_dir/Tela-icon-theme"
-
-    (
-        cd "$tmp_dir/Tela-icon-theme"
-        ./install.sh grey -d ~/.local/share/icons
-    )
+    cd "$tmp_dir/Tela-icon-theme"
+    ./install.sh grey -d ~/.local/share/icons
 }
 
 gtk_themes
