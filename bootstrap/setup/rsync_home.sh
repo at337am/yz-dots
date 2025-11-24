3d99544bc0ea4146705cd46a1e879952289e8299
at337am <lueqit@gmail.com>
2025-11-24 16:59:20 +0800

    WIP

--- bootstrap/setup/rsync_home.sh
+++ bootstrap/setup/rsync_home.sh
@@ -13,7 +13,9 @@ DOTS_PATH="$HOME/workspace/dev/yz-dots/home"
 # 执行同步
 rsync -a "$DOTS_PATH/" ~/
 
-# 解压 p10k 主题
+# todo 这一步不需要, 移动到 tidy 中
 rm -rf ~/.lain/themes/powerlevel10k
+
+# 解压 p10k 主题
 tar -zxf ~/.lain/themes/powerlevel10k.tar.gz -C ~/.lain/themes
 rm -rf ~/.lain/themes/powerlevel10k.tar.gz
