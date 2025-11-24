c91387a00294c568a74fb4161a4d51db31e5895e
at337am <lueqit@gmail.com>
2025-11-24 12:36:47 +0800

    WIP

--- tidy/reset/reset_home.sh
+++ tidy/reset/reset_home.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+# set -euo pipefail
 
-confirm() {
-    local prompt=${1:-"Do you want to continue?"}
-    read -r -p "$prompt [y/N]: " choice
-    case "${choice,,}" in
-        y|yes) return 0 ;;
-        *) return 1 ;;
-    esac
-}
+# confirm() {
+#     local prompt=${1:-"Do you want to continue?"}
+#     read -r -p "$prompt [y/N]: " choice
+#     case "${choice,,}" in
+#         y|yes) return 0 ;;
+#         *) return 1 ;;
+#     esac
+# }
 
-if ! confirm "Are you sure you want to reset home?"; then
-    printf "Operation cancelled. Exiting...\n"
-    exit 1
-fi
+# if ! confirm "Are you sure you want to reset home?"; then
+#     printf "Operation cancelled. Exiting...\n"
+#     exit 1
+# fi
 
-DOTS_PATH="$HOME/workspace/dev/yz-dots/home"
+# DOTS_PATH="$HOME/workspace/dev/yz-dots/home"
 
-# rsync -avh --dry-run --itemize-changes --exclude='.lain/themes' "$DOTS_PATH/" ~/
-rsync -a --exclude='.lain/themes' "$DOTS_PATH/" ~/
+# # rsync -avh --dry-run --itemize-changes --exclude='.lain/themes' "$DOTS_PATH/" ~/
+# rsync -a --exclude='.lain/themes' "$DOTS_PATH/" ~/
 
-# 设置文件权限
-source ~/workspace/dev/yz-dots/fedora_bootstrap/tasks/04_set_permissions.sh
+# # 设置文件权限
+# source ~/workspace/dev/yz-dots/fedora_bootstrap/tasks/04_set_permissions.sh
 
-# 重新软链接
-source ~/workspace/dev/yz-dots/fedora_bootstrap/gui/03_symlinks.sh
+# # 重新软链接
+# source ~/workspace/dev/yz-dots/fedora_bootstrap/gui/03_symlinks.sh
 
-printf "Done.\n"
+# printf "Done.\n"
