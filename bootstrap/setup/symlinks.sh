067c18ac0c688da4f25ac695074652b97dd21d44
at337am <lueqit@gmail.com>
2025-11-24 13:04:17 +0800

    WIP

--- bootstrap/setup/symlinks.sh
+++ bootstrap/setup/symlinks.sh
@@ -2,87 +2,44 @@
 
 set -euo pipefail
 
-DOTS_PATH="$HOME/workspace/dev/yz-dots/home"
+rm -rf ~/.cache/fastfetch
 
-rm -rf ~/tidy
-ln -s "$HOME/workspace/dev/yz-dots/tidy" ~/tidy
-
-rm -rf ~/bin
-ln -s "$DOTS_PATH/bin" ~/bin
-
-rm -rf ~/.config/cava
-ln -s "$DOTS_PATH/.config/cava" ~/.config/cava
-
-rm -rf \
-        ~/.config/fastfetch \
-        ~/.cache/fastfetch
-ln -s "$DOTS_PATH/.config/fastfetch" ~/.config/fastfetch
-
-rm -rf ~/.config/fd
-ln -s "$DOTS_PATH/.config/fd" ~/.config/fd
-
-rm -rf ~/.config/hypr
-ln -s "$DOTS_PATH/.config/hypr" ~/.config/hypr
-
-rm -rf ~/.config/kitty
-ln -s "$DOTS_PATH/.config/kitty" ~/.config/kitty
-
-rm -rf ~/.config/lf
-ln -s "$DOTS_PATH/.config/lf" ~/.config/lf
-
-rm -rf ~/.config/mako
-ln -s "$DOTS_PATH/.config/mako" ~/.config/mako
-
-rm -rf ~/.config/mpv
-ln -s "$DOTS_PATH/.config/mpv" ~/.config/mpv
-
-rm -rf ~/.config/navi
-ln -s "$DOTS_PATH/.config/navi" ~/.config/navi
-
-rm -rf \
-        ~/.config/nvim \
-        ~/.local/share/nvim \
+rm -rf ~/.local/share/nvim \
         ~/.local/state/nvim \
         ~/.cache/nvim
-ln -s "$DOTS_PATH/.config/nvim" ~/.config/nvim
 
-rm -rf ~/.config/rofi
-ln -s "$DOTS_PATH/.config/rofi" ~/.config/rofi
+DOTS_PATH="$HOME/workspace/dev/yz-dots/home"
 
-rm -rf ~/.config/Thunar
-ln -s "$DOTS_PATH/.config/Thunar" ~/.config/Thunar
+ln -sf "$HOME/workspace/dev/yz-dots/tidy" ~/tidy
+ln -sf "$DOTS_PATH/bin" ~/bin
+ln -sf "$DOTS_PATH/.config/cava" ~/.config/cava
+ln -sf "$DOTS_PATH/.config/fastfetch" ~/.config/fastfetch
+ln -sf "$DOTS_PATH/.config/fd" ~/.config/fd
+ln -sf "$DOTS_PATH/.config/hypr" ~/.config/hypr
+ln -sf "$DOTS_PATH/.config/kitty" ~/.config/kitty
+ln -sf "$DOTS_PATH/.config/lf" ~/.config/lf
+ln -sf "$DOTS_PATH/.config/mako" ~/.config/mako
+ln -sf "$DOTS_PATH/.config/mpv" ~/.config/mpv
+ln -sf "$DOTS_PATH/.config/navi" ~/.config/navi
+ln -sf "$DOTS_PATH/.config/nvim" ~/.config/nvim
+ln -sf "$DOTS_PATH/.config/rofi" ~/.config/rofi
+ln -sf "$DOTS_PATH/.config/Thunar" ~/.config/Thunar
+ln -sf "$DOTS_PATH/.config/waybar" ~/.config/waybar
+ln -sf "$DOTS_PATH/.config/yt-dlp" ~/.config/yt-dlp
+ln -sf "$DOTS_PATH/.config/mimeapps.list" ~/.config/mimeapps.list
 
-rm -rf ~/.config/waybar
-ln -s "$DOTS_PATH/.config/waybar" ~/.config/waybar
-
-rm -rf ~/.config/yt-dlp
-ln -s "$DOTS_PATH/.config/yt-dlp" ~/.config/yt-dlp
-
-rm -rf ~/.config/mimeapps.list
-ln -s "$DOTS_PATH/.config/mimeapps.list" ~/.config/mimeapps.list
 
 
 # ------------ zsh START ------------
-rm -rf ~/.lain/lib/aliases.zsh
-rm -rf ~/.lain/lib/func.zsh
+ln -sf "$DOTS_PATH/.lain/lib/aliases.zsh" ~/.lain/lib/aliases.zsh
+ln -sf "$DOTS_PATH/.lain/lib/func.zsh" ~/.lain/lib/func.zsh
 
-ln -s "$DOTS_PATH/.lain/lib/aliases.zsh" ~/.lain/lib/aliases.zsh
-ln -s "$DOTS_PATH/.lain/lib/func.zsh" ~/.lain/lib/func.zsh
-# ------------ zsh END ------------
 
 
 # ------------ fcitx5 START ------------
-rm -rf ~/.local/share/fcitx5/rime/custom_phrase.txt
-rm -rf ~/.local/share/fcitx5/rime/default.yaml
-rm -rf ~/.local/share/fcitx5/rime/yz_pinyin.schema.yaml
-rm -rf ~/.local/share/fcitx5/rime/yz_pinyin.dict.yaml
-rm -rf ~/.local/share/fcitx5/themes
-rm -rf ~/.config/fcitx5
-
-ln -s "$DOTS_PATH/.local/share/fcitx5/rime/custom_phrase.txt" ~/.local/share/fcitx5/rime/custom_phrase.txt
-ln -s "$DOTS_PATH/.local/share/fcitx5/rime/default.yaml" ~/.local/share/fcitx5/rime/default.yaml
-ln -s "$DOTS_PATH/.local/share/fcitx5/rime/yz_pinyin.schema.yaml" ~/.local/share/fcitx5/rime/yz_pinyin.schema.yaml
-ln -s "$DOTS_PATH/.local/share/fcitx5/rime/yz_pinyin.dict.yaml" ~/.local/share/fcitx5/rime/yz_pinyin.dict.yaml
-ln -s "$DOTS_PATH/.local/share/fcitx5/themes" ~/.local/share/fcitx5/themes
-ln -s "$DOTS_PATH/.config/fcitx5" ~/.config/fcitx5
-# ------------ fcitx5 END ------------
+ln -sf "$DOTS_PATH/.local/share/fcitx5/rime/custom_phrase.txt" ~/.local/share/fcitx5/rime/custom_phrase.txt
+ln -sf "$DOTS_PATH/.local/share/fcitx5/rime/default.yaml" ~/.local/share/fcitx5/rime/default.yaml
+ln -sf "$DOTS_PATH/.local/share/fcitx5/rime/yz_pinyin.schema.yaml" ~/.local/share/fcitx5/rime/yz_pinyin.schema.yaml
+ln -sf "$DOTS_PATH/.local/share/fcitx5/rime/yz_pinyin.dict.yaml" ~/.local/share/fcitx5/rime/yz_pinyin.dict.yaml
+ln -sf "$DOTS_PATH/.local/share/fcitx5/themes" ~/.local/share/fcitx5/themes
+ln -sf "$DOTS_PATH/.config/fcitx5" ~/.config/fcitx5
