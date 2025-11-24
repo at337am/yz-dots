4817b27bb67c37396f590ecef9206d2cbb9ff43f
at337am <lueqit@gmail.com>
2025-11-24 13:34:28 +0800

    WIP

--- bootstrap/gui/install_xhs.sh
+++ bootstrap/gui/install_xhs.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# 脚本概述：
+    # 1. 提示用户确认是否继续安装操作。
+    # 2. 清理旧的安装目录和数据目录。
+    # 3. 下载指定版本（2.5）的 XHS-Downloader 源码。
+    # 4. 解压并移动源码到目标安装目录 (/opt/soft/XHS-Downloader)。
+    # 5. 在项目目录下创建并激活 Python 3.12 的虚拟环境。
+    # 6. 使用 `uv` 工具生成依赖锁文件并同步安装依赖。
+    # 7. 创建数据目录的符号链接 (/data/misc/xhs_dl)。
+
 set -euo pipefail
 
 confirm() {
@@ -22,12 +31,6 @@ rm -rf /data/misc/xhs_dl
 
 
 
-# ------------- 下载 python 3.12 -------------
-
-# yay -S --needed python312 --cleanafter
-
-
-
 # ------------- 拉取项目 -------------
 tmp_dir=$(mktemp -d)
 trap 'rm -rf "$tmp_dir"' EXIT
@@ -44,10 +47,15 @@ cd /opt/soft/XHS-Downloader/
 
 rm -f uv.lock
 
-# uv venv -p python3.12 .venv
+
+
+# ------------- 在项目目录下创建并激活虚拟环境 -------------
 
 # 这一步会自动下载 Python 3.12 (如果没安装) 并创建 .venv 目录
 uv venv --python 3.12
+
+# 同步环境
+uv sync
 
 source .venv/bin/activate
 
