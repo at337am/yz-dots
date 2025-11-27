459178c9fbafe6b98fb0f4311b5cda36173bd8b8
at337am <lueqit@gmail.com>
2025-11-27 17:22:25 +0800

    WIP

--- bootstrap/setup/configure_sudo.sh
+++ bootstrap/setup/configure_sudo.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 # 自定义 /etc/sudoers.d/ 中的配置
+# 
+# 大多数 Linux 发行版, sudo 默认会清除大部分环境变量, 包括与代理相关的变量, 
+# 所以即使你在普通用户环境中设置了代理, sudo pacman 也无法使用它
+# 
+# 解决方法
+#   方法一: 使用 sudo 的 -E 选项来继承当前用户环境变量
+#   方法二: 在 /etc/sudoers 文件中允许保留特定变量
 
 set -euo pipefail
 
--- home/.lain/lib/env.zsh
+++ home/.lain/lib/env.zsh
@@ -4,6 +4,9 @@ export BAT_THEME="ansi"
 # trash directory
 export TRASH_DIR="/data/.trash"
 
+# 为 cd 命令设置一个"搜索路径", 让你可以在任何地方直接 cd 到常用项目根目录下的子目录
+# export CDPATH="/data/misc:$HOME/Documents:/data/bak"
+
 export PATH="$HOME/.lain/bin:$PATH"
 export PATH="$HOME/go/bin:$PATH"
 # export PATH="$HOME/.cargo/bin:$PATH"
