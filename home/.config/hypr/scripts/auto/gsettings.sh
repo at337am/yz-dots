ce94e4042a7688560cc6ab57fab5ae069aef2828
at337am <lueqit@gmail.com>
2025-11-27 13:49:54 +0800

    WIP

--- home/.config/hypr/scripts/auto/gsettings.sh
+++ home/.config/hypr/scripts/auto/gsettings.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # 主题名称 = 目录名称
+# 注意: 光标主题除了这里之外, 还需要去 env.conf 里设置
 gsettings set org.gnome.desktop.interface cursor-theme 'Breeze_Light'
 gsettings set org.gnome.desktop.interface cursor-size 24
 gsettings set org.gnome.desktop.interface text-scaling-factor 1.0
