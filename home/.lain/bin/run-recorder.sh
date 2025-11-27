eefe05d7d23db412011d1360f3ad88e7ac1a2772
at337am <lueqit@gmail.com>
2025-11-27 19:50:06 +0800

    WIP

--- home/.lain/bin/run-recorder.sh
+++ home/.lain/bin/run-recorder.sh
@@ -6,19 +6,19 @@ output_dir="$HOME/Videos"
 
 # 检查是否已经在运行, 或者 pgrep -f
 if killall -0 "gpu-screen-recorder" 2>/dev/null; then
-    notify-send -a "recording" \
+    notify-send -a "recorder" \
                 -u low \
                 -h string:x-dunst-stack-tag:volume_notif \
-                "already recording"
+                "Already recording."
     printf "recording in progress.\n"
     exit 1
 fi
 
 mkdir -p "$output_dir"
-output_file="$output_dir/recording_$(date +"%y%m%d_%H%M%S").mkv"
+output_file="$output_dir/recorder_$(date +"%y%m%d_%H%M%S").mkv"
 
 # 发送录制通知
-notify-send -a "recording" \
+notify-send -a "recorder" \
             -u low \
             -h string:x-dunst-stack-tag:volume_notif \
             "Start recording..."
@@ -33,9 +33,9 @@ gpu-screen-recorder -w screen \
                     -o "$output_file"
 
 # 发送录制结束的通知
-notify-send -a "recording" \
+notify-send -a "recorder" \
             -u low \
             -h string:x-dunst-stack-tag:volume_notif \
-            "Stop recording"
+            "Stop recording."
 
 printf "Done.\n"
