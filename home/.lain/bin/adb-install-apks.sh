f90de80e396f944d9f597bc7a67d5c6bf1843034
at337am <lueqit@gmail.com>
2025-12-03 19:51:08 +0800

    WIP

--- home/.lain/bin/adb-install-apks.sh
+++ home/.lain/bin/adb-install-apks.sh
@@ -23,19 +23,21 @@ fail=0
 # 遍历当前目录下所有 .apk 后缀的文件
 for apk in *.apk
 do
-  # 检查是否存在文件
-  [[ ! -f "$apk" ]] && continue
+    # 检查是否存在文件
+    [[ ! -f "$apk" ]] && continue
 
-  if adb install -r "$apk"; then
-    printf "OK -> %s\n" "$apk"
-    success=$((success + 1))
-  else
-    printf "ERR -> %s\n" "$apk"
-    fail=$((fail + 1))
-  fi
-  printf "%s\n" "-------------"
+    if adb install -r "$apk"; then
+        printf "OK -> %s\n" "$apk"
+        success=$((success + 1))
+    else
+        printf "ERR -> %s\n" "$apk"
+        fail=$((fail + 1))
+    fi
+    printf "%s\n" "-------------"
 done
 
 printf "%s\n" "--- Total ---"
 printf "OK count: %d\n" "$success"
 printf "ERR count: %d\n" "$fail"
+
+notify-send "ADB install" "all APKs have been installed."
