5eb15fcd1b724ac3c36c53145b8c01149db90849
at337am <lueqit@gmail.com>
2025-12-05 07:25:09 +0800

    WIP

--- home/.lain/bin/magick-caption.sh
+++ home/.lain/bin/magick-caption.sh
@@ -44,19 +44,22 @@ image_height=$(magick identify -format "%h" "$IMG_PATH")
 font_color="white"
 
 # 字体样式
-font_style="Zhi-Mang-Xing-Regular"
-
-# 字体样式参考
-# IBM-Plex-Sans-Regular
-# IBM-Plex-Sans-Italic
 # Josefin-Sans-Regular
 # Josefin-Sans-Italic
 # Google-Sans-Code-Regular
 # Google-Sans-Code-Italic
 # Zhi-Mang-Xing-Regular
+font_style="Zhi-Mang-Xing-Regular"
 
 # 文字位置
-position="SouthEast" # 右下角
+# Center    居中
+# North     中上方
+# South     中下方
+# NorthWest 左上角
+# NorthEast 右上角
+# SouthWest 左下角
+# SouthEast 右下角
+position="SouthEast"
 
 # 定义文字高度占图片总高度的百分比
 # percentage=0.03
@@ -67,8 +70,10 @@ font_size=$(echo "$image_height * $percentage / 1" | bc)
 
 # 底部边距
 x_offset=$(echo "$font_size * 0.5 / 1" | bc)
-# y_offset=$(echo "$font_size * 0.25 / 1" | bc)
 y_offset=$(echo "$font_size * 0.5 / 1" | bc)
+
+# x_offset=0
+# y_offset=$(echo "$font_size * 1 / 1" | bc)
 
 # 定义输出路径
 dirname=$(dirname "$IMG_PATH")
