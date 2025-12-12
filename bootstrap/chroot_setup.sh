ce8a04034a7a0626b01b688e5f2850d561f0714d
at337am <lueqit@gmail.com>
2025-12-12 11:45:23 +0800

    WIP

--- bootstrap/chroot_setup.sh
+++ bootstrap/chroot_setup.sh
@@ -10,7 +10,7 @@ hwclock --systohc
 
 # 安装一下 nvim 和 vi 编辑器, 最好两个都要安装!
 # 虽然下面直接 sed 编辑了但是最好还是用一下
-pacman -S neovim vi
+pacman -S --needed neovim vi
 
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 sed -i 's/^#zh_CN.UTF-8 UTF-8/zh_CN.UTF-8 UTF-8/' /etc/locale.gen
@@ -34,16 +34,12 @@ useradd -m -G wheel yz
 echo "Please enter password for yz:"
 passwd yz
 
-# 设置用户 sudo 权限:
-pacman -S sudo
-nvim /etc/sudoers   # 或者使用 visudo
-# 在文件中取消以下注释:
-# %wheel ALL=(ALL:ALL) ALL
-
-
+# 设置用户 sudo 权限
+pacman -S --needed sudo
+sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
 # 安装引导程序 GRUB
-pacman -S grub efibootmgr
+pacman -S --needed grub efibootmgr
 
 # 将 GRUB 安装到硬盘的 ESP 分区上
 # 这一步会在 /boot/EFI 下生成一个文件 Arch/grubx64.efi
@@ -54,19 +50,19 @@ grub-mkconfig -o /boot/grub/grub.cfg
 
 
 # 安装驱动和微码
-pacman -S intel-ucode mesa vulkan-intel intel-media-driver
+pacman -S --needed intel-ucode mesa vulkan-intel intel-media-driver
 # 重新生成 GRUB 配置 (为了让 intel-ucode 生效)
 grub-mkconfig -o /boot/grub/grub.cfg
 
 
 # 安装网络服务和 ssh, 方便重启后可以连接 wifi
-pacman -S networkmanager openssh
+pacman -S --needed networkmanager openssh
 # 启动服务
 systemctl enable --now NetworkManager.service
 systemctl enable --now sshd.service
 
 # 提前安装 rsync, 方便后续处理
-pacman -S rsync
+pacman -S --needed rsync
 
 # 最后退出 chroot 环境, 重启
 exit
