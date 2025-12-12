14faf2e70c7760717a5096113969005ede18c0a1
at337am <lueqit@gmail.com>
2025-12-12 12:03:04 +0800

    WIP

--- bootstrap/chroot_setup.sh
+++ bootstrap/chroot_setup.sh
@@ -8,9 +8,19 @@ ln -sf /usr/share/zoneinfo/Asia/Taipei /etc/localtime
 # 同步当前的系统时间到硬件时钟
 hwclock --systohc
 
-# 安装一下 nvim 和 vi 编辑器, 最好两个都要安装!
-# 虽然下面直接 sed 编辑了但是最好还是用一下
-pacman -S --needed neovim vi
+pacman -S --needed \
+    neovim \
+    vi \
+    sudo \
+    grub \
+    efibootmgr \
+    networkmanager \
+    openssh \
+    intel-ucode \
+    mesa \
+    vulkan-intel \
+    intel-media-driver \
+    rsync
 
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 sed -i 's/^#zh_CN.UTF-8 UTF-8/zh_CN.UTF-8 UTF-8/' /etc/locale.gen
@@ -34,10 +44,8 @@ useradd -m -G wheel yz
 echo "Please enter password for yz:"
 passwd yz
 
-# -------------
 
-# 设置用户 sudo 权限
-pacman -S --needed sudo
+# ------------- sudo 配置 -------------
 
 config='# Uncomment to allow members of group wheel to execute any command
 %wheel ALL=(ALL:ALL) ALL
@@ -67,25 +75,14 @@ else
     exit 1
 fi
 
-# -------------
+# --------------------------
 
-# 安装引导程序 GRUB
-pacman -S --needed grub efibootmgr
 
 # 将 GRUB 安装到硬盘的 ESP 分区上
 # 这一步会生成一个文件 /boot/EFI/Arch/grubx64.efi
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch
+
 # 生成配置文件
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# 安装驱动和微码
-pacman -S --needed intel-ucode mesa vulkan-intel intel-media-driver
-# 重新生成 GRUB 配置 (为了让 intel-ucode 生效)
-grub-mkconfig -o /boot/grub/grub.cfg
-
-# 安装网络服务和 ssh, 方便重启后可以连接 wifi
-pacman -S --needed networkmanager openssh
 systemctl enable NetworkManager.service sshd.service
-
-# 提前安装 rsync, 方便后续处理
-pacman -S --needed rsync
