532eb43f848ed1ce7c99df4c3903d90a289b126a
at337am <lueqit@gmail.com>
2025-12-13 16:45:37 +0800

    WIP

--- bootstrap/gui/setup_grub.sh
+++ bootstrap/gui/setup_grub.sh
@@ -18,7 +18,7 @@ set_grub_key() {
     local regex="^[#[:space:]]*${key}[[:space:]]*="
 
     # 检查该键是否存在 (无论是否被注释)
-    if grep -q "$regex" "$grub_config"; then
+    if grep -E -q "$regex" "$GRUB_CONFIG"; then
         echo "更新配置: ${key}=${value}"
         # 使用 sed 替换整行
         sudo sed -i "s|${regex}.*|${key}=${value}|" "$grub_config"
@@ -38,6 +38,6 @@ set_grub_key "GRUB_SAVEDEFAULT" "true"
 # 禁用子菜单
 set_grub_key "GRUB_DISABLE_SUBMENU" "y"
 
-sudo grub-mkconfig -o /boot/grub/grub.cfg
+# sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 printf "Done.\n"
