171ab9eae386d42fd2f406990651fd4b092603cd
at337am <lueqit@gmail.com>
2025-12-18 18:41:46 +0800

    WIP

--- home/.config/hypr/scripts/rofi/prompts.sh
+++ home/.config/hypr/scripts/rofi/prompts.sh
@@ -14,13 +14,11 @@ fi
 
 selected_file=$( (cd "$notes_path" && ls -1) | rofi -dmenu -i -p "prompts" -theme ~/.config/rofi/themes/prompts.rasi)
 
-if [ -z "$selected_file" ]; then
-    exit 0
-fi
+[[ -z "$selected_file" ]] && exit 0
 
 full_path="$notes_path/$selected_file"
 
-if [ -f "$full_path" ]; then
+if [[ -f "$full_path" ]]; then
     wl-copy < "$full_path"
     notify-send -a "clipboard" \
                 -u low \
