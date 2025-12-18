bd71e0224dc0869ebaff8bdcf0560d2e2f1cc1df
at337am <lueqit@gmail.com>
2025-12-18 18:36:33 +0800

    WIP

--- home/.config/hypr/scripts/rofi/clipboard.sh
+++ home/.config/hypr/scripts/rofi/clipboard.sh
@@ -12,7 +12,7 @@ cliphist list | rofi -dmenu -i -p "clipboard" | cliphist decode | wl-copy
 #     notify-send -a "clipboard" \
 #                 -u low \
 #                 -h string:x-dunst-stack-tag:volume_notif \
-#                 "📋  Copied"
+#                 "Copied"
 # else
 #     notify-send -a "clipboard" \
 #                 -u low \
--- home/.config/hypr/scripts/rofi/prompts.sh
+++ home/.config/hypr/scripts/rofi/prompts.sh
@@ -25,7 +25,7 @@ if [ -f "$full_path" ]; then
     notify-send -a "clipboard" \
                 -u low \
                 -h string:x-dunst-stack-tag:volume_notif \
-                "📋  Copied"
+                "Copied"
 else
     notify-send -a "clipboard" \
                 -u low \
