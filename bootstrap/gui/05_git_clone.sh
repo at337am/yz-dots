3ac46c7d80a820bbca8b2ea4c484f41979b93a46
at337am <lueqit@gmail.com>
2026-02-06 14:12:31 +0800

    WIP

--- bootstrap/gui/05_git_clone.sh
+++ bootstrap/gui/05_git_clone.sh
@@ -14,6 +14,7 @@ git_clone() {
     git clone git@github.com:at337am/notes.git
     git clone git@github.com:at337am/yz-dots.git
     git clone git@github.com:at337am/skit.git
+    git clone git@github.com:at337am/hello.git
     git clone git@github.com:at337am/raindrop.git
 }
 
