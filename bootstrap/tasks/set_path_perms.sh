54ca8e5eb0ddea29c0f1ef01b2f3e38ee26977e6
at337am <lueqit@gmail.com>
2026-02-08 17:15:15 +0800

    WIP

--- bootstrap/tasks/set_path_perms.sh
+++ bootstrap/tasks/set_path_perms.sh
@@ -1,20 +1,11 @@
 #!/usr/bin/env bash
 
-# 设置相关文件权限
+# 设置一些路径的权限
 
 set -euo pipefail
 
-zsh() {
-    chmod 600 ~/.zshrc
-    chmod 600 ~/.zprofile
-}
-
-# ------------- 如果脚本只接收到一个参数, 并且匹配 -------------
-
-if [[ "$#" -eq 1 && "$1" == "zsh" ]]; then
-    zsh
-    exit 0
-fi
+chmod 600 ~/.zshrc
+chmod 600 ~/.zprofile
 
 chmod 600 ~/.gitconfig
 
@@ -22,5 +13,3 @@ chmod 700 ~/.ssh
 chmod 600 ~/.ssh/config
 chmod 600 ~/.ssh/id_rsa
 chmod 644 ~/.ssh/id_rsa.pub
-
-zsh
