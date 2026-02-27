effc0ec894bf7bde9ab66bfebdedcc9d6d6753b6
at337am <lueqit@gmail.com>
2026-02-27 17:59:09 +0800

    WIP

--- home/.lain/bin/run-xhs.sh
+++ home/.lain/bin/run-xhs.sh
@@ -8,28 +8,70 @@ if ! command -v "uv" &> /dev/null; then
     exit 1
 fi
 
+# ----------------------------------
+# 函数: 打印帮助信息
+# ----------------------------------
+usage() {
+    printf "Usage:\n"
+    printf "  %s [flags]\n" "$(basename "$0")"
+    printf "\nFlags:\n"
+    printf "  -c, --clear           清除历史记录\n"
+    printf "  -s, --start           启动 (默认)\n"
+    printf "  -h, --help            Show this help message\n"
+}
+
+# 项目目录
 TARGET_DIR="/opt/soft/XHS-Downloader"
 
+# 检查项目目录是否存在
 if [[ ! -d "$TARGET_DIR" ]]; then
     printf "Error: %s does not exist.\n" "$TARGET_DIR" >&2
     exit 1
 fi
 
+# 进入项目目录
 cd "$TARGET_DIR"
 
-# 如果有参数, 则清除下载记录
-if [[ "$#" -eq 1 && "$1" == "clear" ]]; then
-
+# 清除历史记录
+clear_history() {
     rm -rfv \
-    Volume/ExploreID.db \
-    Volume/MappingData.db \
-    Volume/Download/ExploreData.db \
-    Volume/Temp
+        Volume/ExploreID.db \
+        Volume/MappingData.db \
+        Volume/Download/ExploreData.db \
+        Volume/Temp
+}
 
-    printf "Done.\n"
-    exit 0
+# 启动程序
+start_app() {
+    uv run python main.py
+}
+
+# 参数个数不能大于 1
+if [[ "$#" -gt 1 ]]; then
+    printf "${RED}Error:${NC} Too many arguments.\n" >&2
+    usage >&2
+    exit 1
 fi
 
-uv run python main.py
+# 如果 $1 为空 (无参数)，则赋值为 --start
+action="${1:---start}"
+
+case "$action" in
+    -c|--clear)
+        clear_history
+        ;;
+    -s|--start)
+        start_app
+        ;;
+    -h|--help)
+        usage
+        exit 0
+        ;;
+    *)
+        printf "${RED}Error:${NC} Unknown flag %s\n" "$action" >&2
+        usage >&2
+        exit 1
+        ;;
+esac
 
 printf "Done.\n"
