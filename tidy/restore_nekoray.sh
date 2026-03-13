3c8b03d7124c028c572f584ff0af4f9797dfad6a
at337am <lueqit@gmail.com>
2026-03-13 09:19:49 +0800

    WIP

--- tidy/restore_nekoray.sh
+++ tidy/restore_nekoray.sh
@@ -2,9 +2,22 @@
 
 set -euo pipefail
 
+# 定义颜色
+RED='\033[0;31m'        # 红色
+NC='\033[0m'            # 重置色
+
 restore_path="$HOME/.local/share/restore/nekoray.tar.gz"
 old_restore_path="$HOME/.local/share/restore/old/nekoray_bak_$(date +"%y%m%d_%H%M%S").tar.gz"
 nekoray_path="/opt/soft/nekoray"
+
+usage() {
+    printf "Usage:\n"
+    printf "  %s [flags]\n" "$(basename "$0")"
+    printf "\nFlags:\n"
+    printf "  -b, --bak             备份整个 nekoray\n"
+    printf "  -r, --restore         重置 nekoray (默认)\n"
+    printf "  -h, --help            Show this help message\n"
+}
 
 confirm() {
     local prompt=${1:-"Do you want to continue?"}
@@ -57,14 +70,34 @@ bak() {
 	tar -zcf "$restore_path" -C /opt/soft/ nekoray
 }
 
-if [[ "$#" -eq 0 ]]; then
-    restore
-elif [[ "$#" -eq 1 && "$1" == "bak" ]]; then
-    bak
-else
-	printf "Error: Invalid arguments.\n" >&2
-	printf "Usage: %s [bak]\n" "$(basename "$0")" >&2
+# -------------- 程序主入口 --------------
+
+# 参数个数不能大于 1
+if [[ "$#" -gt 1 ]]; then
+    printf "${RED}Error:${NC} Too many arguments.\n" >&2
+    usage >&2
     exit 1
 fi
+
+# 如果 $1 为空 (无参数)，则赋值为 --restore
+action="${1:---restore}"
+
+case "$action" in
+    -b|--bak)
+		bak
+        ;;
+    -r|--restore)
+        restore
+        ;;
+    -h|--help)
+        usage
+        exit 0
+        ;;
+    *)
+        printf "${RED}Error:${NC} Unknown flag %s\n" "$action" >&2
+        usage >&2
+        exit 1
+        ;;
+esac
 
 printf "Done.\n"
