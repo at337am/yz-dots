3aa1d0f2695da177933fbaefde248d45842501c0
at337am <lueqit@gmail.com>
2026-03-29 10:09:41 +0800

    WIP

--- home/.lain/lib/env.zsh
+++ home/.lain/lib/env.zsh
@@ -1,7 +1,11 @@
 # 注意: 敏感的环境变量不要直接写这里!
 # 请用 source .env 文件 的方式调用
 
+# bat theme
 export BAT_THEME="ansi"
+
+# fzf theme
+export FZF_DEFAULT_OPTS="--color=bg+:#565656,pointer:#c9bed0"
 
 # 为 cd 命令设置一个"搜索路径", 让你可以在任何地方直接 cd 到常用项目根目录下的子目录
 # export CDPATH="/data/misc:$HOME/Documents:/data/bak"
