521d76ce2d378c4c3f52bce467fd034e3e161c4c
at337am <lueqit@gmail.com>
2026-04-08 09:26:39 +0800

    WIP

--- home/.lain/lib/env.zsh
+++ home/.lain/lib/env.zsh
@@ -7,9 +7,6 @@ export BAT_THEME="ansi"
 # fzf theme
 export FZF_DEFAULT_OPTS="--color=bg+:#565656,pointer:#c9bed0"
 
-# 为 cd 命令设置一个"搜索路径", 让你可以在任何地方直接 cd 到常用项目根目录下的子目录
-# export CDPATH="/data/misc:$HOME/Documents:/data/bak"
-
 export PATH="$HOME/.lain/bin:$PATH"
 export PATH="$HOME/go/bin:$PATH"
 
